91a62de0b4af2516e9fb59e14e99facd0ec22812
Gustavo Henrique <gustavohgustavo@hotmail.com>
2022-11-18 10:10:23 -0300

    Envio de notificação por SMS

--- backend/src/main/java/com/gustavonascimento/dsmeta/controles/ControllerVendas.java
+++ backend/src/main/java/com/gustavonascimento/dsmeta/controles/ControllerVendas.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gustavonascimento.dsmeta.dominio.Vendas;
 import com.gustavonascimento.dsmeta.servicos.ServicosVendas;
+import com.gustavonascimento.dsmeta.servicos.SmsServices;
 
 @RestController
 @RequestMapping(value="/vendas")
@@ -17,6 +19,8 @@ public class ControllerVendas {
 
 	@Autowired
 	private ServicosVendas servVendas;
+	@Autowired
+	private SmsServices sms;
 	
 	@GetMapping
 	public Page<Vendas> encontrarVendas(
@@ -25,5 +29,10 @@ public class ControllerVendas {
 			Pageable pageable)
 	{
 		return servVendas.todasAsVendas(minDate,maxDate,pageable);
+	}
+	@GetMapping("/{id}/notificacao")
+	public void notificarSms(@PathVariable Long id)
+	{
+		sms.sendSms(id);
 	}
 }
